f548bfcf9be1b0863acfafb301f6d3a0b6d7c4f4
Eyzic <einar@ingemarsson.nu>
2021-02-19 01:05:17 +0100

    Added a text field which calls the API to retrieve data from the database

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import DefaultInput from './defaultInput';
+import DisplayValue from './displayValue';
 
 export default function App() {
 
-  let info = 'Filip!';
+  let info = 'Värde i databasen: ';
   console.log("Application running!");
+
+  /* fetch('http://localhost:5000')
+     .then(response => response.json())
+     .then((data) => {
+       info += data.value;
+       console.log(data);
+       console.log(info);
+ 
+     })
+     .catch((error) => {
+       console.error(error);
+     }); */
+
   return (
     <View style={styles.container}>
       <DefaultInput>Placeholder</DefaultInput>
-      <Text>Netinfo: {info}</Text>
+      <DisplayValue />
       <StatusBar style="auto" />
     </View>
   );
