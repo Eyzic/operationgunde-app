a374fc78a1c8d8df5bdb20d92260388dfcfe4678
jemapr <jessica.p@live.se>
2021-02-22 00:22:12 +0100

    lagt till logga, samt namn på app, ändra färger

--- App.js
+++ App.js
@@ -2,25 +2,39 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from 'react-native';
 import DefaultInput from './defaultInput';
+import { Image } from 'react-native';
 
 export default function App() {
-
-  let info = 'Frida!';
+  let info = 'Operation Gunde';
   console.log("Application running!");
   return (
     <View style={styles.container}>
-      <DefaultInput>Placeholder</DefaultInput>
-      <Text>Netinfo: {info}</Text>
+      <Image
+        style={styles.Logo}
+        source={require('/Users/Jessica/Desktop/Chalmers/Kandidatarbete/operationgunde-app/167086572.png')}
+      />
+      <Text style={styles.Rubrik} >
+        {info}
+        </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  Logo: {
+    width: 250,
+    height: 250,
+  },
   container: {
     flex: 1,
-    backgroundColor: 'hsla(200, 50%, 50%,1)',
+    backgroundColor: 'hsla(205, 80%, 82%,1)',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Rubrik: {
+    fontFamily: "MarkerFelt-Wide",
+    fontSize: 30,
+  }
+  
 });
