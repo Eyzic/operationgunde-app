142da61d55c31077e3afcb1620ca37da945a79e0
fridakrohn <krohnf@student.chalmers.se>
2021-02-26 15:00:36 +0100

    mkt bra mkt skit

--- App.js
+++ App.js
@@ -1,26 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from 'react-native';
+import { StyleSheet, Button, Text, Image, TextInput, TouchableWithoutFeedback, View , SafeAreaView, TouchableOpacity, Alert, Platform, Dimensions} from 'react-native';
 import DefaultInput from './defaultInput';
-
+import { useDimensions, useDeviceOrientation} from '@react-native-community/hooks';
 export default function App() {
 
-  let info = 'historik sida';
-  console.log("Application running!");
+  let info = 'historik';
+  //console.log("Application running!");
+
+
+  
   return (
-    <View style={styles.container}>
-      <DefaultInput>Placeholder</DefaultInput>
-      <Text>Netinfo: {info}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+
+<view style={{backgroundColor: 'hsla(0, 50%, 50%,1)',
+     width: 100,
+     height: 70,
+     alignSelf: "flex-start",
+  }
+}>
+ 
+    </view>
+
+    <view style={{
+      backgroundColor: 'hsla(200, 50%, 50%,1)' ,
+       width: 70,
+       height: 90,
+       
+       }}>
+ 
+    </view>
+
+   
+  
+    
+    </SafeAreaView>
   );
 }
+
+
+
+// mina popups fungerar inte? Alert
+
+//utmaxat från view
+// <DefaultInput>Placeholder</DefaultInput>
+//<Text>Netinfo: {info}</Text>
+//<StatusBar style="auto" />
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'hsla(285, 50%, 50%,1)',
-    alignItems: 'center',
-    justifyContent: 'center',
+     flexDirection: "row",
+    backgroundColor: 'hsla(264, 16%, 47%,1)',
+    //'hsla(285, 50%, 50%,1)'
+   // alignItems: 'center',
+    justifyContent: 'center' ,
+    alignItems: "center" ,//secondary axis
+    //paddingTop: Platform.OS == "ios"? 20 :30,
   },
 });
