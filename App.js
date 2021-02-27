9d16f8b86cc1ba3f2a0e5063714b5b2710787048
jemapr <jessica.p@live.se>
2021-02-28 00:03:06 +0100

    Lagt till knappar och några nya paket

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TextInput, TouchableWithoutFeedback, View, Image } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableWithoutFeedback, View, Image, Button, SafeAreaView, Alert } from 'react-native';
 import DefaultInput from './defaultInput';
 
 export default function App() {
-  let info = 'Operation Gunde';
+  let info = 'Operation Gunde' ;
+  let space = ' ';
   console.log("Application running!");
   return (
     <View style={styles.container}>
@@ -16,9 +17,29 @@ export default function App() {
         {info}
         </Text>
       <StatusBar style="auto" />
+
+      <Text style={styles.Rubrik} >
+        {space}
+      </Text>
+
+      <Text style={styles.Rubrik} >
+        {space}
+      </Text>
+
+      <Button
+        title="Logga in"
+        onPress={() => Alert.alert('Funktion kommer snart')}
+      />
+      
+      <Button
+        title="Registrera dig"
+        onPress={() => Alert.alert('Funktion kommer snart')}
+      />
+
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   Logo: {
