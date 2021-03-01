5efd6eeaf529f58b75b074f903e6bcd4552f887f
fridakrohn <krohnf@student.chalmers.se>
2021-03-01 15:01:32 +0100

    fungerande ram

--- App.js
+++ App.js
@@ -4,35 +4,56 @@ import { StyleSheet, Button, Text, Image, TextInput, TouchableWithoutFeedback, V
 import DefaultInput from './defaultInput';
 import { useDimensions, useDeviceOrientation} from '@react-native-community/hooks';
 export default function App() {
+  
 
-  let info = 'historik';
+    let info = 'historik sida';
+    console.log("Application running!");
+    return (
+
+      <SafeAreaView style={styles.container}>
+      <View style={{
+        backgroundColor: 'hsla(264, 16%, 100%,1)',
+        height: '95%',
+        width: '95%',
+        borderRadius: 10,
+
+       
+      }}>
+        <Button 
+        
+        color="blue"
+  title="Solid Button"
+/>
+<Button
+  title="2"
+/>
+      </View>
+      </SafeAreaView>
+
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'hsla(264, 16%, 47%,1)',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  //let info = 'historik';
   //console.log("Application running!");
 
 
   
-  return (
-    <SafeAreaView style={styles.container}>
+  //return (
+   // <SafeAreaView style={styles.container}>
 
-
-<view style={{backgroundColor: 'hsla(0, 50%, 100%,1)',
-     height:'95%',
-     width:'98%',
-     borderRadius: 10,
-     
-  }
-}>
- <Button style={styles.btnSize}
-   title= "knapp"
-   >
-            
-          </Button>
-
-    </view>
     
     
-    </SafeAreaView>
-  );
-}
+ //   </SafeAreaView>
+  //);
+//}
 
 
 
@@ -43,24 +64,23 @@ export default function App() {
 //<Text>Netinfo: {info}</Text>
 //<StatusBar style="auto" />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-     flexDirection: "row",
-    backgroundColor: 'hsla(264, 16%, 47%,1)',
+//const styles = StyleSheet.create({
+  //container: {
+    //flex: 1,
+    //backgroundColor: 'hsla(200, 50%, 50%,1)',
+    //alignItems: 'center',
+  //  justifyContent: 'center',
+    //flex: 1,
+    // flexDirection: "row",
+   // backgroundColor: 'hsla(264, 16%, 47%,1)',
     //'hsla(285, 50%, 50%,1)'
-    alignItems: 'center',
-    justifyContent: 'center' ,
-    alignItems: "center" ,//secondary axis
+   // alignItems: 'center',
+    //justifyContent: 'center' ,
+    //alignItems: "center" ,//secondary axis
    // paddingTop: Platform.OS == "ios"? 20 :30,
-  },
+ // },
 
-  btnContainer:{
-    flexDirection:"row",
-    justifyContent:"space-between"
-},
-btnSize:{
-    width:50
-},
 
-});
+
+
+//});
