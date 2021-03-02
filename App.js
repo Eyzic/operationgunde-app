a3e517586458231dddbf1a8ca4efb5b8c7b26562
fridakrohn <krohnf@student.chalmers.se>
2021-03-02 15:40:02 +0100

    Faktisk rätt fin

--- App.js
+++ App.js
@@ -6,40 +6,104 @@ import { useDimensions, useDeviceOrientation} from '@react-native-community/hook
 export default function App() {
   
 
-    let info = 'historik sida';
-    console.log("Application running!");
+  
+    
     return (
 
       <SafeAreaView style={styles.container}>
-      <View style={{
-        backgroundColor: 'hsla(264, 16%, 100%,1)',
+      <View style={ {
+        
+        backgroundColor: 'hsla(240, 13%, 15%,1)',
         height: '95%',
-        width: '95%',
+        width: '98%',
         borderRadius: 10,
+        zIndex: 0,
+        position: 'absolute'
 
-       
+      }}>
+
+        
+
+{/* <View  style={ {
+        flexDirection: "row",
+        alignItems: 'center',
+        justifyContent: 'center',
       }}>
         <Button 
-        
-        color="blue"
-  title="Solid Button"
+        color="green"
+  title="måndag 1/3"
+  
 />
-<Button
-  title="2"
-/>
-      </View>
-      </SafeAreaView>
 
+<Button
+  title="tisdag 1/3"
+/>
+</View>
+
+<View>
+        <Button style={styles.buttonsar}
+  title="måndag 2/3"
+/>
+
+<Button
+  title="onsdag 2/3"
+/>
+</View> */}
+      </View>
+
+      <View style={ {
+        
+        backgroundColor: 'hsla(240, 13%, 0%,0.5)',
+        height: '20%',
+        width: '96%',
+        borderRadius: 10,
+        zIndex: 1,
+        position: 'absolute',
+        top: '4%'
+
+      }}>
+        <Text style={{
+          fontSize: devicePixelRatio*50,
+          color: 'hsla(342, 78%, 72%,1)',
+          zIndex:2,
+          position: 'relative',
+          textAlign:'center',
+          
+          
+          
+        
+        }}>
+          operationGunde
+        </Text>
+</View>
+      </SafeAreaView>
     );
   }
   
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: 'hsla(264, 16%, 47%,1)',
+    
+      backgroundColor: 'hsla(240, 14%, 11%,1)',
       alignItems: 'center',
       justifyContent: 'center',
     },
+    
+    buttonsar: {
+      //display: 'flex',
+      height: 50,
+      borderRadius: 5,
+     // justifyContent: 'center',
+     // alignItems: 'center',
+
+      backgroundColor: 'black',
+     // shadowColor: '#2AC062',
+     // shadowOpacity: 0.4,
+     // shadowOffset: { height: 10, width: 0 },
+     // shadowRadius: 20,
+  },
+
+
   });
   //let info = 'historik';
   //console.log("Application running!");
