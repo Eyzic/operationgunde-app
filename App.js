c8c4d78e4293964da2678f882a692c28351ed34b
jemapr <jessica.p@live.se>
2021-03-21 22:42:21 +0100

    Lagt till hur träningsveckan sett ut

--- App.js
+++ App.js
@@ -21,15 +21,15 @@ export default function App() {
           {'Din \ndagsform '}
         </Text>
         <Image source={require( './Springa.png')}
-          style={{width: 100, height: 100, left:240, top: -100}} />
-        <Text style={{fontFamily: "MarkerFelt-Wide",fontSize: 20, top:-90, left: 10}} >
+          style={{width: 125, height: 125, left:230, top: -95}} />
+        <Text style={{fontFamily: "MarkerFelt-Wide",fontSize: 20, top:-115, left: 10}} >
           {'Taggad och redo'}
         </Text>
       </View>
 
       <View style={{ flex: 0.35, backgroundColor: "'hsla(272, 100%, 97%,1)'" }} >
       <ScrollView horizontal = {true} >
-        <Text style={styles.text}>
+        <Text style={styles.text2}>
           {'Denna text går att skrolla i sidled, här kommer vi lägga in dom olika målen '}
           {'personen har och hur långt man kommit med dom i procent tror jag, tex dagens steg, '} 
           {'hur långt man har kommit med antalet timmar man planerat att träna, antal km man tänkt springa'}
@@ -39,10 +39,42 @@ export default function App() {
       </View>
 
       <View style={{ flex: 0.04, backgroundColor: "'hsla(268, 67%, 89%,1)'" }} />
-      <View style={{ flex: 0.7, backgroundColor: "'hsla(272, 100%, 97%,1)'" }} />
+
+      <View style={{ flex: 0.4, backgroundColor: "'hsla(272, 100%, 97%,1)'" }} >
+        <Text style ={styles.text}>
+          {'\nVecka 12'}
+          {'\n \nMån     Tis     Ons     Tor      Fre      Lö       Sö'}
+        </Text>
+      </View>
+    
+      <View  style={styles.row} >
+          <View  style={[styles.box2, { backgroundColor: "'hsla(272, 100%, 97%,1)'"  }]} />
+          <View style={[styles.box, { backgroundColor: "skyblue" }]} />
+          <View  style={[styles.box2, { backgroundColor: "'hsla(272, 100%, 97%,1)'"  }]} />
+          <View style={[styles.box, { backgroundColor: "skyblue" }]} />
+          <View  style={[styles.box2, { backgroundColor: "'hsla(272, 100%, 97%,1)'"  }]} />
+          <View style={[styles.box, { backgroundColor: "skyblue" }]} />
+          <View  style={[styles.box2, { backgroundColor: "'hsla(272, 100%, 97%,1)'"  }]} />
+          <View style={[styles.box, { backgroundColor: "skyblue" }]} />
+          <View  style={[styles.box2, { backgroundColor: "'hsla(272, 100%, 97%,1)'"  }]} />
+          <View style={[styles.box, { backgroundColor: "skyblue" }]} />
+          <View  style={[styles.box2, { backgroundColor: "'hsla(272, 100%, 97%,1)'"  }]} />
+          <View style={[styles.box, { backgroundColor: "skyblue" }]} />
+          <View  style={[styles.box2, { backgroundColor: "'hsla(272, 100%, 97%,1)'"  }]} />
+          <View style={[styles.box, { backgroundColor: "skyblue" }]} />
+    
+      </View>
+      <View style={{ flex: 0.07, backgroundColor: "'hsla(272, 100%, 97%,1)'" }} />
       <View style={{ flex: 0.04, backgroundColor: "'hsla(268, 67%, 89%,1)'" }} />
-      <View style={{ flex: 0.7, backgroundColor: "'hsla(272, 100%, 97%,1)'" }} />
-      <View style={{ flex: 0.30, backgroundColor: "'hsla(0, 0%, 100%,1)'" }} />
+      <View style={{ flex: 0.9, backgroundColor: "'hsla(272, 100%, 97%,1)'" }} />
+
+      <View style={styles.menu} >
+      <Button
+        title=" Menyrad"
+        onPress={() => Alert.alert('Funktion kommer snart')}
+      />
+
+      </View>
 
     </View>
   );
@@ -65,6 +97,32 @@ const styles = StyleSheet.create({
   text: {
     fontFamily: "MarkerFelt-Wide",
     fontSize: 20,
-  }
+    left: 25,
+  },
+  text2: {
+    fontFamily: "MarkerFelt-Wide",
+    fontSize: 20,
+    left: 15,
+  },
+  box: {
+    width: 30,
+    height: 30,
+  },
+
+  box2: {
+    width: 26,
+    height: 35,
+  },
+
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
   
+  menu: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    flex: 0.30, 
+    backgroundColor: 'hsla(0, 0%, 100%,1)',
+  },
 });
