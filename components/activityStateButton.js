b17f44238db48dbe0a63a79aea102b043cad8c02
Eyzic <einar@ingemarsson.nu>
2021-04-06 12:20:57 +0200

    Moved stopWatch to a separate file and fixed som minor bugs

--- components/activityStateButton.js
+++ components/activityStateButton.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Text, Dimensions, PixelRatio, StyleSheet, ScrollView, Platform, Alert, View, TouchableOpacity, Image } from 'react-native';
+import StopWatch from './stopWatch';
 
 const {
     width: SCREEN_WIDTH,
@@ -16,29 +17,11 @@ function normalize(size) {
     }
 }
 
-function formatTime(time) {
-    let hours = Math.floor((time / (60 * 60 * 1000) % 60));
-    hours = leftPad(hours, 2);
-
-    let minutes = Math.floor((time / (60 * 1000)) % 60);
-    minutes = leftPad(minutes, 2);
-
-    let seconds = Math.floor((time / 1000) % 60);
-    seconds = leftPad(seconds, 2);
-
-    console.log("SEC: " + seconds);
-
-    let displayTime = hours + ":" + minutes + ":" + seconds;
-    return displayTime;
-}
-
-const leftPad = (value, length) => value.toString().length < length ? leftPad("0" + value, length) : value;
-
 const activityStateButton = (props) => {
     const [activityOn, setActivityOn] = useState(props.activity);
 
     const [time, setTime] = useState();
-    const [timer, setTimer] = useState(new stopWatch());
+    const [timer, setTimer] = useState(new StopWatch());
 
     const [startTime, setStartTime] = useState();
     const [interval, refInterval] = useState();
@@ -53,7 +36,7 @@ const activityStateButton = (props) => {
     }, [activityOn]);
 
     useEffect(() => {
-        let formatedTime = () => formatTime(timer.getTime());
+        let formatedTime = () => timer.getTime();
         let i = setInterval(() => setTime(formatedTime), 100);
         //return clearInterval(i);
 
@@ -75,39 +58,5 @@ const styles = StyleSheet.create({
     },
 
 });
-
-class stopWatch {
-    constructor() {
-        this.startTime = null;
-        this.running = false;
-        this.stopTime = null;
-        this.interval = null;
-        this.currentTime = 0;
-    }
-
-    start() {
-        if (!this.running) {
-            this.startTime = Date.now();
-            this.running = true;
-            this.interval = setInterval(this.time.bind(this), 100);
-        }
-    };
-
-    time() {
-        this.currentTime = Date.now() - this.startTime;
-        console.log("TIME: " + this.currentTime);
-    }
-
-    getTime() { return this.currentTime }
-
-    pause() {
-        clearInterval(this.interval);
-        this.stopTime = Date.now();
-        this.running = false;
-        console.log(Date.now());
-    };
-
-
-}
 
 export default activityStateButton;
