01cee3649ca5ec35fa20a12de0943ea8bffc816f
fridakrohn <krohnf@student.chalmers.se>
2021-05-06 16:36:55 +0200

    styles och städ

--- src/components/goal.js
+++ src/components/goal.js
@@ -2,14 +2,11 @@ import React from "react";
 import {
   Text,
   View,
-  TouchableWithoutFeedback,
-  Button,
   Platform,
   Dimensions,
   PixelRatio,
   StyleSheet,
 } from "react-native";
-import StatsDisplay from "./statsDisplay";
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 const scale = SCREEN_WIDTH / 320;
@@ -26,51 +23,15 @@ function normalize(size) {
 const Goal = (props) => {
   return (
     <View style={[styles.container]}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-          alignContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "hsla(316, 47%, 73%, 1)",
-            width: normalize(30),
-            height: normalize(30),
-            alignContent: "center",
-            alignItems: "center",
-            justifyContent: "center",
-            borderRadius: normalize(13),
-          }}
-        >
-          <View
-            style={{
-              backgroundColor: "hsla(0,0%,100%,0.7)",
-              width: normalize(24),
-              height: normalize(24),
-              alignContent: "center",
-              borderRadius: normalize(10),
-            }}
-          ></View>
+      <View style={[styles.positions]}>
+        <View style={[styles.checkbox]}>
+          <View style={[styles.filled]}></View>
         </View>
         <View style={([styles.title], { paddingLeft: normalize(20) })}>
           <Text style={styles.h4}> {props.goalType} </Text>
         </View>
       </View>
-      <View
-        style={{
-          backgroundColor: "#DB9ACA",
-          width: normalize(100),
-          height: 35,
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-          borderRadius: normalize(13),
-          alignContent: "center",
-        }}
-      >
+      <View style={[styles.valueBox]}>
         <Text style={{ color: "white", fontFamily: "monospace" }}>
           {props.value}
         </Text>
@@ -88,6 +49,42 @@ const styles = StyleSheet.create({
     alignContent: "center",
     alignItems: "center",
     height: 50,
+  },
+
+  positions: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+  },
+
+  checkbox: {
+    backgroundColor: "hsla(316, 47%, 73%, 1)",
+    width: normalize(30),
+    height: normalize(30),
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: normalize(13),
+  },
+
+  filled: {
+    backgroundColor: "hsla(0,0%,100%,0.7)",
+    width: normalize(24),
+    height: normalize(24),
+    alignContent: "center",
+    borderRadius: normalize(10),
+  },
+
+  valueBox: {
+    backgroundColor: "#DB9ACA",
+    width: normalize(100),
+    height: 35,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: normalize(13),
+    alignContent: "center",
   },
 });
 
--- src/components/userInf.js
+++ src/components/userInf.js
@@ -2,15 +2,12 @@ import React, { useState } from "react";
 import {
   Text,
   View,
-  TouchableWithoutFeedback,
-  Button,
   Platform,
   Dimensions,
   PixelRatio,
   StyleSheet,
   TextInput,
 } from "react-native";
-import StatsDisplay from "./statsDisplay";
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 const scale = SCREEN_WIDTH / 320;
@@ -25,38 +22,18 @@ function normalize(size) {
 }
 
 const UserInf = (props) => {
-  // if ({props.done}==='ja') {
-  //     filled="hsla(0,0%,100%,0.7)",
-  //    }
-  //  else: {
-  //  filled='transparent',
-  // },
-  //const [text, setText] = useState("");
   return (
     <View style={[styles.container]}>
       <View
         style={{
           flexDirection: "row",
-          // justifyContent: "center",
           alignContent: "center",
           alignItems: "center",
         }}
       >
         <Text style={styles.h4}> {props.infoType}: </Text>
       </View>
-      <View
-        style={{
-          backgroundColor: "#DB9ACA",
-          // width: SCREEN_WIDTH * 0.9,
-          height: normalize(30),
-          flexDirection: "row",
-          alignItems: "center",
-          //  justifyContent: "center",
-          borderRadius: normalize(13),
-          // alignContent: "center",
-          paddingLeft: normalize(10),
-        }}
-      >
+      <View style={[styles.infobox]}>
         <Text style={{ color: "white" }}>{props.value}</Text>
       </View>
     </View>
@@ -69,41 +46,19 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-between",
-    // alignContent: "center",
-    //alignItems: "center",
     paddingBottom: normalize(10),
     height: 50,
   },
-  title: {
-    flexDirection: "row",
-    //padding: 10,
-    // justifyContent: "space-between",
-    backgroundColor: "#DB9ACA",
-  },
-
-  statsRow: {
-    backgroundColor: "hsla(324, 53%, 66%,0.65)",
-    flexDirection: "row",
-    borderTopRightRadius: normalize(15),
-    borderBottomLeftRadius: 0,
-    padding: 1,
-  },
-  time: {
-    alignItems: "center",
-    justifyContent: "space-around",
-    flexGrow: 1,
-    margin: 10,
-    padding: 10,
-  },
-  h2: {
-    fontSize: normalize(20),
-  },
-  h3: {
-    color: "white",
-    fontSize: normalize(15),
-  },
   h4: {
     fontSize: normalize(18),
+  },
+  infobox: {
+    backgroundColor: "#DB9ACA",
+    height: normalize(30),
+    flexDirection: "row",
+    alignItems: "center",
+    borderRadius: normalize(13),
+    paddingLeft: normalize(10),
   },
 });
 
