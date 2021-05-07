69f82a40a49837063f47d8bc298d67e2edf4c147
Eyzic <einar@ingemarsson.nu>
2021-05-07 16:03:40 +0200

    Fixed wrong path to Sizes.js

--- src/screens/groups/friendGroupFriendScreen.js
+++ src/screens/groups/friendGroupFriendScreen.js
@@ -7,7 +7,7 @@ import FriendProfile from '../../components/friendProfile';
 import StandardTemplate from '../../templates/StandardTemplate';
 
 import Style from '../../styles/Style';
-import Sizes from '../../styles/Sizes';
+import Sizes from '../../styles/sizes';
 
 const {
     width: SCREEN_WIDTH,
